66659ddea648f52779e5bf2f68d12694fc8c3a3d
Hans Fredrik Furholt <hansff@gmail.com>
2021-03-19 13:02:30 +0100

    init

---
+++ aws-logs.el
@@ -0,0 +1,24 @@
+;;; aws-logs.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2021 Hans Fredrik Furholt
+;;
+;; Author: Hans Fredrik Furholt <https://github.com/hansffu>
+;; Created: March 19, 2021
+;; Modified: March 19, 2021
+;; Version: 0.0.1
+;; Keywords: Symbol’s value as variable is void: finder-known-keywords
+;; Homepage: https://github.com/hansffu/aws-logs
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+
+
+
+(provide 'aws-logs)
+;;; aws-logs.el ends here
